2031e6c9f81a15c00291a579aa62b5580e5ebf89
Ricardo Razura <ricardorazest@gmail.com>
2025-04-01 16:48:35 -0700

    Ricardo: Agregado de Icono al boton Iniciar sesion

--- src/components/PrincipleButton.tsx
+++ src/components/PrincipleButton.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { ReactNode } from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   className?: string;
+  icon?: ReactNode;
 }
 
-export default function PrincipleButton({title, className, ...props}: ButtonProps) {
+export default function PrincipleButton({title, className, icon, ...props}: ButtonProps) {
   return (
     <button className={`text-black rounded-md border-1 py-2 px-6 ${className}`} {...props}>
+      {icon && <span className="w-6 h-6">{icon}</span>}
       {title}
     </button>
   );
